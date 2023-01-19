01618847f6613f2d9d96ea2ce9bddd5ba3e41dad
Thuy Trang Nguyen <thuytrangnguyen@MacBook-Pro.local>
2023-01-19 13:16:24 -0500

    added utils.js and writing posts

--- api/posts.js
+++ api/posts.js
@@ -1,5 +1,41 @@
 const express = require('express');
 const postRouter = express.Router();
+const { requireUser } = require('./utils');
+const { createPost } = require('../db/index.js');
+
+postRouter.post('/', requireUser, async (req, res, next) => {
+  const { title, content, tags = "" } = req.body;
+
+  const tagArr = tags.trim().split(/\s+/)
+  const postData = {};
+
+  // only send the tags if there are some to send
+  if (tagArr.length) {
+    postData.tags = tagArr;
+  }
+
+  try {
+    const user = requireUser();
+    //console.log('authorId', authorId);
+    postData.user = user;
+    postData.title = title;
+    postData.content = content;
+    const post = await createPost(postData);
+    console.log(post);
+    if (post) {
+      res.send({ post });
+    }
+    else {
+      next({name: 'create post error', message: 'no post'});
+    }
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
+
+postRouter.post('/', requireUser, async (req, res, next) => {
+  res.send({ message: 'under construction' });
+});
 
 postRouter.use((req, res, next) => {
     console.log("A request is being made to /posts");
