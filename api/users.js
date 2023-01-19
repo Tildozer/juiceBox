dc55d62eeddb6a3597c720d78e9672c03d5574ad
Thuy Trang Nguyen <thuytrangnguyen@MacBook-Pro.local>
2023-01-19 12:16:14 -0500

    create users

--- api/users.js
+++ api/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const usersRouter = express.Router();
 const { getUserByUsername } = require('../db/index');
 const jwt = require('jsonwebtoken');
+const { createUser } = require('../db/index');
 
 usersRouter.use((req, res, next) => {
   console.log("A request is being made to /users");
@@ -51,6 +52,43 @@ usersRouter.post('/login', async (req, res, next) => {
     console.log(error);
     next(error);
   }
+});
+
+usersRouter.post('/register', async (req, res, next) => {
+  const { username, password, name, location } = req.body;
+
+  try {
+    const _user = await getUserByUsername(username);
+    console.log(_user);
+
+    if (_user) {
+      next({
+        name: 'UserExistsError',
+        message: 'A user by that username already exists'
+      });
+    }
+
+    const user = await createUser({
+      username,
+      password,
+      name,
+      location,
+    });
+
+    const token = jwt.sign({ 
+      id: user.id, 
+      username
+    }, process.env.JWT_SECRET, {
+      expiresIn: '1w'
+    });
+
+    res.send({ 
+      message: "thank you for signing up",
+      token 
+    });
+  } catch ({ name, message }) {
+    next({ name, message })
+  } 
 });
 
 module.exports = usersRouter;
